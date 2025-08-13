bbef72235ade4e356e2fc57672f98dc6bcb8d0f8
Dsprog <dsprog@gmail.com>
2025-08-12 23:36:39 -0300

    chat bot mais historico

--- app.py
+++ app.py
@@ -2,15 +2,98 @@ from flask import Flask,render_template, request, Response
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+from time import sleep
+from helper import carrega, salva
+from selecionar_persona import personas, selecionar_persona
+from gerenciar_historico import remover_mensagens_mais_antigas
 
 load_dotenv()
 
 CHAVE_API_GOOGLE = os.getenv("GEMINI_API_KEY")
-MODELO_ESCOLHIDO = "gemini-1.5-flash"   
+MODELO_ESCOLHIDO = "gemini-1.5-flash"
 genai.configure(api_key=CHAVE_API_GOOGLE)
 
 app = Flask(__name__)
 app.secret_key = 'alura'
+
+contexto = carrega("dados/musimart.txt")
+
+def criar_chatbot():
+    personalidade = "neutro"
+
+    prompt_do_sistema = f"""
+    # PERSONA
+
+    Você é um chatbot de atendimento a clientes de um e-commerce.
+    Você não deve responder perguntas que não sejam dados do ecommerce informado!
+
+    Você deve utilizar apenas dados que estejam dentro do 'contexto'
+
+    # CONTEXTO
+    {contexto}
+
+    # PERSONALIDADE
+    {personalidade}
+
+    # Histórico
+    Acesse sempre o histórico de mensagens, e recupere informações ditas anteriormente.
+    """
+
+    configuracao_modelo = {
+        "temperature" : 0.1,
+        "max_output_tokens" : 8192
+    }
+
+    llm = genai.GenerativeModel(
+        model_name=MODELO_ESCOLHIDO,
+        system_instruction=prompt_do_sistema,
+        generation_config=configuracao_modelo
+    )
+
+    chatbot = llm.start_chat(history=[])
+
+    return chatbot
+
+chatbot = criar_chatbot()
+
+def bot(prompt):
+    maximo_tentativas = 1
+    repeticao = 0
+
+    while True:
+        try:
+
+            personalidade = personas[selecionar_persona(prompt)]
+
+            mensagem_usuario = f"""
+            Considere esta personalidade para responder a mensagem:
+            {personalidade}
+
+            Responda a seguinte mensagem, sempre lembrando do histórico:
+            {prompt}
+            """
+
+            resposta = chatbot.send_message(mensagem_usuario)
+
+            if len(chatbot.history) > 10:
+                chatbot.history = remover_mensagens_mais_antigas(chatbot.history)
+
+            print(f"quantidade: {len(chatbot.history)}\n {chatbot.history}")
+
+            return resposta.text
+
+        except Exception as erro:
+            repeticao += 1
+            if repeticao >= maximo_tentativas:
+                return "Erro no Gemini: %s" % erro
+
+            sleep(50)
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    prompt = request.json["msg"]
+    resposta = bot(prompt)
+    return resposta
 
 @app.route("/")
 def home():
